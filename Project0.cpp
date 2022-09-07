71392bf273bc3e561d33c02c4489c3e8849166e7
VioletShi-_- <passionatefrisbee@gmail.com>
2022-09-06 22:33:23 -0400

    adding space to separate chunks of code

--- Project0.cpp
+++ Project0.cpp
@@ -6,6 +6,7 @@ int main() {
     const char* mid_print = R"(    printf("    const char* head_print = R\"(%%s)\";\n", head_print);%c    printf("    const char* mid_print = R\"(%%s)\";\n", mid_print);%c    printf("    const char* end_print = R\"(%%s)\";\n", end_print);%c    printf("    const char* end = \"%%s\";\n", end);%c)";
     const char* end_print = R"(    printf(head_print, '\n', '\n');%c    printf(mid_print, '\n', '\n', '\n', '\n');%c    printf(end_print, '\n', '\n', '\n', '\n');%c    printf(end, '\n', '\n');%c)";
     const char* end = "    return 0;%c}";
+    
     printf(head, '\n', '\n', '\n');
     printf("    const char* head = \"%s\";\n", head);
     printf("    const char* head_print = R\"(%s)\";\n", head_print);
@@ -16,5 +17,6 @@ int main() {
     printf(mid_print, '\n', '\n', '\n', '\n');
     printf(end_print, '\n', '\n', '\n', '\n');
     printf(end, '\n', '\n');
+
     return 0;
 }
