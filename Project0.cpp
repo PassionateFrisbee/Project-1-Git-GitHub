d0ff7a4f3c88ca9fa5ab55b164cf93f1c9ea44a7
VioletShi-_- <passionatefrisbee@gmail.com>
2022-09-06 22:38:02 -0400

    adding comments to create conflicts

--- Project0.cpp
+++ Project0.cpp
@@ -1,11 +1,13 @@
 #include <cstdio>
 
 int main() {
+    //data: store the information of the code
     const char* head = "#include <cstdio>%c%cint main() {%c";
     const char* head_print = R"(    printf(head, '\n', '\n', '\n');%c    printf("    const char* head = \"%%s\";\n", head);%c)";
     const char* mid_print = R"(    printf("    const char* head_print = R\"(%%s)\";\n", head_print);%c    printf("    const char* mid_print = R\"(%%s)\";\n", mid_print);%c    printf("    const char* end_print = R\"(%%s)\";\n", end_print);%c    printf("    const char* end = \"%%s\";\n", end);%c)";
     const char* end_print = R"(    printf(head_print, '\n', '\n');%c    printf(mid_print, '\n', '\n', '\n', '\n');%c    printf(end_print, '\n', '\n', '\n', '\n');%c    printf(end, '\n', '\n');%c)";
     const char* end = "    return 0;%c}";
+    //print statements: print the information in order
     printf(head, '\n', '\n', '\n');
     printf("    const char* head = \"%s\";\n", head);
     printf("    const char* head_print = R\"(%s)\";\n", head_print);
